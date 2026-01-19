4ebb66b73636e688ea021781c60e84e0b0e9e6cc
Henry <halmeida@ecuasanitas.com>
2026-01-19 16:21:04 -0500

    feat(Estrucutra): Se restructuro las carpetas y se coloco el tema claro

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,41 @@
-import 'package:admin/constants.dart';
-import 'package:admin/controllers/menu_app_controller.dart';
-import 'package:admin/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
+import 'resource/theme/theme.dart';
+import 'states/menu_app_controller.dart';
+import 'states/theme_controller.dart';
+import 'ui/core/main_screen.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => MenuAppController(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ThemeController(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final themeController = context.watch<ThemeController>();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Admin Panel',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: bgColor,
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white),
-        canvasColor: secondaryColor,
-      ),
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => MenuAppController(),
-          ),
-        ],
-        child: MainScreen(),
-      ),
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: themeController.themeMode,
+      home: const MainScreen(),
     );
   }
 }
